75f44e7bb88f82e1d54136a5de3b4762f43ba30b
sgn1 <surajn.vnit@gmail.com>
2020-07-22 22:38:28 -0700

    add add method

---
+++ funmath/sum.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/davecgh/go-spew/spew"
+)
+
+func Add(a, b int) int {
+	spew.Dump(a, b)
+	return a + b
+}
+
+func main() {
+	spew.Dump(Add(1, 2))
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sgn1/gomodfun
+
+go 1.14
+
+require github.com/davecgh/go-spew v1.1.1
